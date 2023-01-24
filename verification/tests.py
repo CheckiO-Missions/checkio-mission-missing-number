f5bc6c2ba6913389336537aa91fb4e52e896a091
Oleksandr Zozulia <freemanlex@gmail.com>
2023-01-24 20:11:28 +0000

    upd

--- verification/tests.py
+++ verification/tests.py
@@ -12,21 +12,21 @@ TESTS = {
     "Basics": [
         {
             "input": [[1, 4, 2, 5]],
-            "answer": 3
+            "answer": 3,
         },
         {
             "input": [[2, 6, 8]],
-            "answer": 4
+            "answer": 4,
         },
     ],
     "Extra": [
         {
             "input": [[5, 25, 30, 20, 15]],
-            "answer": 10
+            "answer": 10,
         },
         {
             "input": [[0, 1, 3, 4, 2, 6, 9, 7, 8]],
-            "answer": 5
+            "answer": 5,
         },
     ]
 }
